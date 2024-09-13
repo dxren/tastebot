3a9a5b9e810a75623fa3c60942d38e2744cf524e
Steven <ssebexen@gmail.com>
2024-09-13 08:14:28 -0400

    stash

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -1,11 +1,9 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
-const data = new SlashCommandBuilder()
+export const metadata = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("Replies with Pong!");
 
-const execute = async (interaction: ChatInputCommandInteraction) => {
+export const execute = async (interaction: ChatInputCommandInteraction) => {
   await interaction.reply("Pong!");
 };
-
-export { data, execute };
